cdd13491ace4a57a9aff56c0a9efa88fc5c65568
Michael Awad <awad.michael@216.nl>
2018-06-01 16:50:22 +0200

    WIP

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan')
 // var cookieParser = require('cookie-parser')
 // var bodyParser = require('body-parser')
 var session = require('express-session')
-var FileStore = require('session-file-store')(session) 
+var FileStore = require('session-file-store')(session)
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+const bodyParser = require('body-parser')
+var User = require('./models/user')
+
+var router = express.Router()
+
+router.use(bodyParser.json())
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource')
+})
 
-module.exports = router;
+router.post('/signup', function (req, res, next) {
+  User.findOne({username: req.body.username})
+    .then((user) => {
+      if (user != null) {
+        var err = new Error('User' + req.body.username + 'alreadt exists')
+        err.status = 403
+        next(err)
+      } else {
+        return User.create({
+          username: req.body.username,
+          password: req.body.password
+        })
+      }
+    })
+    .then((user) => {
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json')
+      res.json({status: 'Registration Successful', user: user})
+    },
+    (err) => next(err))
+    .catch((err) => next(err))
+})
+
+module.exports = router
