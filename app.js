a8694c2f9b160a34d78154ab7ef24c57cfb16e18
Awadje <michaelawad85@gmail.com>
2018-06-02 23:07:35 +0200

    Implement passport JWTstrategy

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan')
 // var bodyParser = require('body-parser')
 var session = require('express-session')
 var FileStore = require('session-file-store')(session)
+var passport = require('passport')
+var authenticate = require('./authenticate')
+var config = require('./config')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
@@ -19,7 +22,7 @@ const mongoose = require('mongoose')
 mongoose.Promise = require('bluebird')
 
 // Connection URL
-const url = 'mongodb://localhost:27017/conFusion'
+const url = config.mongoUrl
 const connect = mongoose.connect(url, {
   /* other options */
 })
@@ -36,57 +39,14 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 // app.use(cookieParser('12345-67890-09876-54321'))
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}))
 
-function auth (req, res, next) {
-  console.log(req.session)
-
-  if (!req.session.user) {
-    var authHeader = req.headers.authorization
-
-    if (!authHeader) {
-      var err = new Error('You are not not authenticated')
-      res.setHeader('WWW-Authenticate', 'Basic')
-      err.status = 401
-      return next(err)
-    }
-
-    var auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
-    var username = auth[0]
-    var password = auth[1]
-
-    if (username === 'admin' && password === 'password') {
-      req.session.user = 'admin'
-      next()
-    } else {
-      res.setHeader('WWW-Authenticate', 'Basic')
-      err.status = 401
-      return next(err)
-    }
-  } else {
-    if (req.session.user === 'admin') {
-      next()
-    } else {
-      err = new Error('You are not not authenticated')
-      res.setHeader('WWW-Authenticate', 'Basic')
-      err.status = 401
-      return next(err)
-    }
-  }
-}
-
-app.use(auth)
-
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(passport.initialize())
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use('/dishes', dishRouter)
 app.use('/promotions', promoRouter)
 app.use('/leaders', leaderRouter)
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express')
 const bodyParser = require('body-parser')
-var User = require('./models/user')
+var User = require('../models/user')
+var passport = require('passport')
+var authenticate = require('../authenticate')
 
 var router = express.Router()
 
@@ -11,27 +13,40 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource')
 })
 
-router.post('/signup', function (req, res, next) {
-  User.findOne({username: req.body.username})
-    .then((user) => {
-      if (user != null) {
-        var err = new Error('User' + req.body.username + 'alreadt exists')
-        err.status = 403
-        next(err)
+router.post('/signup', (req, res, next) => {
+  User.register(new User({username: req.body.username}),
+    req.body.password, (err, user) => {
+      if (err) {
+        res.statusCode = 500
+        res.setHeader('Content-Type', 'application/json')
+        res.json({err: err})
       } else {
-        return User.create({
-          username: req.body.username,
-          password: req.body.password
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200
+          res.setHeader('Content-Type', 'application/json')
+          res.json({success: true, status: 'Registration Successful!'})
         })
       }
     })
-    .then((user) => {
-      res.statusCode = 200
-      res.setHeader('Content-Type', 'application/json')
-      res.json({status: 'Registration Successful', user: user})
-    },
-    (err) => next(err))
-    .catch((err) => next(err))
+})
+
+router.post('/login', passport.authenticate('local'), (req, res, next) => {
+  var token = authenticate.getToken({_id: req.user._id})
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+  res.json({success: true, token: token, status: 'Login Successful!'})
+})
+
+router.get('/logout', (req, res, next) => {
+  if (req.session) {
+    req.session.destroy()
+    res.clearCookie('session-id')
+    res.redirect('/')
+  } else {
+    var err = new Error('You are not logged in!')
+    err.status = 403
+    next(err)
+  }
 })
 
 module.exports = router
