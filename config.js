d3680565c3653c8a1ce37f62a9887958138dcb0c
Michael Awad <31645628+MichaelAwad216@users.noreply.github.com>
2018-06-11 09:40:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   'secretKey': '12345',
   'mongoUrl': 'mongodb://localhost:27017/',
   'facebook': {
-    clientId: '294081064464608',
-    clientSecret: 'd3e6974cf19484e2ae5de0f2e00fdde0'
+    clientId: '',
+    clientSecret: ''
   }
 }
